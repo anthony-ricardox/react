58b2820308c04c82efd8c2b11803d44ea1ba7938
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-03-06 22:06:39 -0300

    aprendendo a usar jsx e por style inline com react

--- ferramenta-front/src/App.jsx
+++ ferramenta-front/src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
+const titulo = <h1>Esse é um Titulo</h1>
+const styleP = {  
+    color: 'green',
+}
 
-const App = () => {  
-  return <div>  App React </div>
+const App = () => {
+  const random = Math.random() 
+  const ativo = false
+  return (
+  <>
+    {titulo}
+    <p style={styleP}> {new Date().getFullYear() }</p>
+  <p className={ativo ? 'Ativo' : 'Inativo'}>{random * 1}</p>
+  </>)
 }
     
 
