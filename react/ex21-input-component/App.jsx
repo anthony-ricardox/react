abc3de67bc6fcd76f433048fd8e8242850d6c596
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-04-21 19:51:17 -0300

    componente Checkbox

--- react/ex21-input-component/App.jsx
+++ react/ex21-input-component/App.jsx
@@ -4,6 +4,7 @@ import Input from "./Form/Input";
 import "../public/style.css";
 import Select from "./Form/Select";
 import Radio from "./Form/Radio";
+import Chekbox from "./Form/Chekbox";
 
 const App = () => {
   const [nome, setNome] = React.useState("");
@@ -11,6 +12,11 @@ const App = () => {
   const [produto, setProduto] = React.useState("");
   const [cor, setCor] = React.useState("");
   const [fruta, setFruta] = React.useState("");
+  const [linguagens, setLinguagens] = React.useState([]);
+  const [termos, setTermos] = React.useState([])
+  if(termos.length > 0 ){ 
+    console.log('Enviado')
+  }
 
   return (
     <form action="">
@@ -34,10 +40,28 @@ const App = () => {
         required
       />
       <button>Enviar</button>
-        <h2>Cores</h2>
+      <h2>Cores</h2>
       <Radio options={["Azul", "Verde"]} value={cor} setValue={setCor} />
-        <h2>Frutas</h2>
-       <Radio options={["Laranja", "Uva", "Melão"]} value={fruta} setValue={setFruta} />
+      <h2>Frutas</h2>
+      <Radio
+        options={["Laranja", "Uva", "Melão"]}
+        value={fruta}
+        setValue={setFruta}
+      />
+
+      <h2>Termos</h2>
+      <Chekbox
+        options={["Li e aceito os Termos"]}
+        value={termos}
+        setValue={setTermos}
+      />
+
+      <h2>Checkbox</h2>
+      <Chekbox
+        options={["JavaScript", "PHP", "Ruby"]}
+        value={linguagens}
+        setValue={setLinguagens}
+      />
     </form>
   );
 };
