eb777ad2ca1684375f5613da0ae6655eade3b84b
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-05-19 22:58:25 -0300

    Animation CSS

---
+++ react/ex25-animation/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Produto from './Produto'
+import './App.css'
+
+const App = () => {
+  const [ativo, setAtivo] = React.useState(false)
+  return (
+    
+    <div>
+        <button onClick={() => setAtivo(!ativo)}> {ativo ?'Ativado' : 'Desativado'  }</button>
+        {ativo && <Produto/>}
+    </div>
+  )
+}
+
+export default App
