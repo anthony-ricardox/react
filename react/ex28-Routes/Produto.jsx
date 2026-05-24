ae7021ed6646fb6929f2e94f8389722d2c2fdddf
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-05-24 17:34:33 -0300

    Outra rrota aninhada, sem ser dinamica

--- react/ex28-Routes/Produto.jsx
+++ react/ex28-Routes/Produto.jsx
@@ -1,34 +1,20 @@
 import React from "react";
-import {
-  useLocation,
-  useParams,
-  Routes,
-  Route,
-  NavLink,
-} from "react-router-dom";
-import ProdutoDescricao from "./ProdutoDescricao";
-import ProdutoAvaliacao from "./ProdutoAvaliacao";
-import ProdutoCustomizado from "./ProdutoCustomizado";
+import { useLocation, useParams, NavLink , Outlet} from "react-router-dom";
 
 const Produto = () => {
   const param = useParams();
-  const location = useLocation();
-  const search = new URLSearchParams(location.search);
-  console.log(search.get("q"));
+  
+  
   return (
     <div>
+        
       <h1>Produto:{param.id}</h1>
       <nav>
         <NavLink to={`/produto/${param.id}`}>Descrição</NavLink> {""}
         <NavLink to={`/produto/${param.id}/avaliacao`}>Avaliação</NavLink> {""}
         <NavLink to={`/produto/${param.id}/customizado`}>Customizado</NavLink>
       </nav>
-
-      <Routes>
-        <Route path='/' element={<ProdutoDescricao />} />
-        <Route path="avaliacao" element={<ProdutoAvaliacao />} />
-        <Route path="customizado" element={<ProdutoCustomizado />} />
-      </Routes>
+      <Outlet/>
     </div>
   );
 };
