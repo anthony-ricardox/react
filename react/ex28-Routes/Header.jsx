e0eccaadf57fcec0218eff253cb156486d82e53a
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-05-24 17:16:25 -0300

    rotas aninhadas

--- react/ex28-Routes/Header.jsx
+++ react/ex28-Routes/Header.jsx
@@ -3,13 +3,12 @@ import { NavLink, useLocation } from "react-router-dom";
 import "./Header.module.css";
 const Header = () => {
   const active = ({ isActive }) => (isActive ? { color: "green" } : {});
-  const location = useLocation()
- 
-  React.useEffect(()=> {
-    console.log(`Mudou a rota `)
-  }, [location])
+  const location = useLocation();
+
+  React.useEffect(() => {
+    console.log(`Mudou a rota `);
+  }, [location]);
   return (
-    
     <nav>
       <NavLink to="/" end style={active}>
         Home
@@ -17,7 +16,9 @@ const Header = () => {
       <NavLink style={active} to="sobre">
         Sobre
       </NavLink>
-      <NavLink style={active} to="login">Login</NavLink>
+      <NavLink style={active} to="login">
+        Login
+      </NavLink>
       <NavLink style={active} to="contato">
         Contato
       </NavLink>
--- react/ex28-Routes/Produto.jsx
+++ react/ex28-Routes/Produto.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import {
+  useLocation,
+  useParams,
+  Routes,
+  Route,
+  NavLink,
+} from "react-router-dom";
+import ProdutoDescricao from "./ProdutoDescricao";
+import ProdutoAvaliacao from "./ProdutoAvaliacao";
+import ProdutoCustomizado from "./ProdutoCustomizado";
 
 const Produto = () => {
-  const param = useParams()
-  const location = useLocation()
-  const search = new URLSearchParams(location.search)
-  console.log(search.get('q'))
+  const param = useParams();
+  const location = useLocation();
+  const search = new URLSearchParams(location.search);
+  console.log(search.get("q"));
   return (
     <div>
-
       <h1>Produto:{param.id}</h1>
+      <nav>
+        <NavLink to={`/produto/${param.id}`}>Descrição</NavLink> {""}
+        <NavLink to={`/produto/${param.id}/avaliacao`}>Avaliação</NavLink> {""}
+        <NavLink to={`/produto/${param.id}/customizado`}>Customizado</NavLink>
+      </nav>
+
+      <Routes>
+        <Route path='/' element={<ProdutoDescricao />} />
+        <Route path="avaliacao" element={<ProdutoAvaliacao />} />
+        <Route path="customizado" element={<ProdutoCustomizado />} />
+      </Routes>
     </div>
   );
 };
