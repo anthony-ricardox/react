ba7841ba6280e613163eedf57aeff31a073be8be
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-18 14:16:33 -0300

    utilizando filter para renderizar itens específicos

--- react - santander/my-app/src/components/List.tsx
+++ react - santander/my-app/src/components/List.tsx
@@ -9,8 +9,78 @@ const List = () => {
     "Affogato",
   ];
 
+  interface Cafe {
+    name: string;
+    description: string;
+    preparationTimes: number;
+  }
+
+  const cafes: Cafe[] = [
+    {
+      name: "Iced Caramel Macchiato",
+      description: "Rico e com toque de baunilha",
+      preparationTimes: 5,
+    },
+    {
+      name: "Caffé Mocha",
+      description: "um classico Strbuckes",
+      preparationTimes: 5,
+    },
+    {
+      name: "Starbucks Blond Vanilla Late",
+      description: "Suave e aveludado",
+      preparationTimes: 3,
+    },
+    {
+      name: "Sparking Expresso",
+      description: "Um toque brilhante",
+      preparationTimes: 2,
+    },
+    {
+      name: "Affogato",
+      description: "o mais gostoso da terra",
+      preparationTimes: 1,
+    },
+  ];
+
   const listItems = coffees.map((cafe) => <li key={cafe}> {cafe}</li>);
-  return <ul  style={{textAlign: 'left'}}>{listItems}</ul>;
+  const lista = cafes.map((cafee) => (
+    <div key={cafee.name}>
+      {" "}
+      <h5> {cafee.name}</h5>
+      <p style={{ fontSize: "10px", paddingBottom: '10px' }}>
+        {cafee.description}{" "}
+        <span style={{ fontWeight: "bold" }}>
+          {" "}
+          {` ${cafee.preparationTimes}min `}
+        </span>
+      </p>
+    </div>
+  ));
+
+  const topSale = cafes.filter(
+    (cafe) => ['Sparking Expresso', 'Caffé Mocha'].includes(cafe.name)
+  );
+  const topTime = cafes.filter((cafe) => cafe.preparationTimes >= 3)
+  console.log('Top Vendas', topSale, 'Time' ,topTime);
+  return (
+    <>
+      <ul>
+          <h4 style={{textAlign: "left"}}>Mais vendidos</h4>
+          {topSale.map((top) => <li style={{textAlign: "left" , fontSize: "12px"}} key={top.name} > {top.name}</li>)}
+      </ul>
+      <br />
+
+      <ul style={{ textAlign: "left" }}>
+        <h4>Cafés</h4>
+        {listItems}
+        <br />
+        <h4>Descrição</h4>
+        {lista}
+        </ul>;
+        
+    </>
+  );
 };
 
 export default List;
