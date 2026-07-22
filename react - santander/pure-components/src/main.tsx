5ddbcd8ad81566162cd071e8ea9d2d464d0a6d19
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-22 01:57:06 -0300

    adiciona componente Side-Effects

--- react - santander/pure-components/src/main.tsx
+++ react - santander/pure-components/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from '../src/atividade/App'
+import App from './atividade/MutacaoLocal'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
