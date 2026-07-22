402b867ec02004fc0843025ac85967923ad1c970
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-22 00:43:58 -0300

    funcões em react + typescript, criando um componente que calcule o dobro de um numero

---
+++ react - santander/pure-components/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import DoubleNumber from './components/DoubleNumber'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1> Pure Components: React</h1>
+     <DoubleNumber number={2}/>
+    </>
+  )
+}
+
+export default App
