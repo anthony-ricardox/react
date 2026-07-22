6ee9794e45f9bd42802fcc704ebaffdf4296bb48
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-22 01:38:13 -0300

    Side effects (efeitos colaterais) consequências (não) intencionais

--- react - santander/pure-components/src/App.tsx
+++ react - santander/pure-components/src/App.tsx
@@ -9,12 +9,7 @@ function App() {
   return (
     <>
      <h1> Pure Components: React</h1>
-     <DoubleNumber number={2}/>
-     <DoubleNumber number={5}/>
-     <DoubleNumber number={10}/>
-     <SquareNumber number={2}/>
-     <SquareNumber number={5}/>
-     <SquareNumber number={10}/>
+     
     </>
   )
 }
