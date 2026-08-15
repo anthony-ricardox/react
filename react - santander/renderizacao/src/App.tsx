df9f0a5a0d929fd37f95e8291cfcdea0b5af08b8
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-08-15 15:53:56 -0300

    Retornando condicionalmente nada com null

--- react - santander/renderizacao/src/App.tsx
+++ react - santander/renderizacao/src/App.tsx
@@ -1,22 +1,28 @@
-
-import './App.css'
-import Item from './items/Item'
+import "./App.css";
+import Item from "./items/Item";
 
 function App() {
-
+  const itemsList = [
+    { id: "meias", name: "Meias", check: true },
+    { id: "escova-de-dente", name: "Escova de dente", check: false },
+    { id: "carregador-celular", name: "Carregador de celular", check: false },
+    { id: "pasta-de-dente", name: "Pasta de dente", check: true },
+  ];
 
   return (
     <>
       <div className="list">
         <h1>Itens para colocar na mala</h1>
-        <Item name='Meias' check/>
-        <Item name='Geladeira'/>
-        <Item name='Cama'/>
-        <Item name='Roupas' check/>
-
+        {itemsList.map((item) => {
+          if(item.check){ 
+            return null;
+          }
+          
+          return <Item name={item.name} check={item.check} key={item.id} />;
+        })}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
