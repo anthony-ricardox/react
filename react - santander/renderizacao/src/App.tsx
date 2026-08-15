e383577512c73db9bee22052b9bbd68db0db954d
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-08-15 15:31:29 -0300

    utilizando renderização condicional com check

--- react - santander/renderizacao/src/App.tsx
+++ react - santander/renderizacao/src/App.tsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 import './App.css'
 import Item from './items/Item'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
       <div className="list">
         <h1>Itens para colocar na mala</h1>
-        <Item name='Meias'/>
+        <Item name='Meias' check/>
         <Item name='Geladeira'/>
         <Item name='Cama'/>
-        <Item name='Roupas'/>
+        <Item name='Roupas' check/>
 
       </div>
     </>
